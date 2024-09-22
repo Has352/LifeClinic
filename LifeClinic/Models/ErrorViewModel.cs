c9c8d70ce89c6bb53eae7fe71620d84ff489bc89
hasan <hasanabuahmed@gmail.com>
2024-09-22 22:11:35 +0300

    Add project files.

---
+++ LifeClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LifeClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
